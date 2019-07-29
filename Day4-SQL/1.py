411bf8b66aa61e19ddc870f9e96eaef8de149433
Javier Chames <haimch@gmail.com>
2019-07-29 15:32:38 +0300

    fixed

--- Day4-SQL/1.py
+++ Day4-SQL/1.py
@@ -1,5 +1,5 @@
 import pymysql
-from bottle import run, route, get, post, delete, put,request
+from bottle import run, route, get, post, delete, put,request,template
 import json
 
 connection =pymysql.connect(host='localhost',
@@ -8,13 +8,13 @@ connection =pymysql.connect(host='localhost',
                             db='bootcamp_2019',
                             cursorclass=pymysql.cursors.DictCursor)
 
-@get('/students')
+@get('/')
 def getAll():
   try:
     with connection.cursor() as cursor:
         query = "SELECT * FROM student"
         cursor.execute(query)
-        return json.dumps(cursor.fetchall())
+        return  json.dumps(cursor.fetchall())
   except:
          return json.dumps({'error':'something is wrong with the DB'})
                        
@@ -26,12 +26,12 @@ def get(name):
                 query = f'SELECT * FROM student WHERE firstName ="{name}"'
                 cursor.execute(query)
         return json.dumps(cursor.fetchall())
-   except:
-         return json.dumps({'error':'something is wrong with the DB'})
+   except  Exception as e:
+         return json.dumps({'error':'something is wrong with the DB'+repr(e)})
  
  
  
-@post('/add')
+@post('/add_student')
 def add():
     try:
         with connection.cursor() as cursor:
@@ -44,26 +44,53 @@ def add():
                 query = "INSERT into student (firstName, lastName,course,start_study_year,end_study_year)values('{}', '{}','{}','{}','{}')".format(fname, lname,course,start,end)
                 cursor.execute(query)
                 connection.commit()
-                
-                cursor.execute(query)
-                connection.commit()
                 student_query = "SELECT * FROM student"
                 cursor.execute(student_query)
-                return json.dumps(cursor.fetchall())
-    except:    
-        return json.dumps({'error':'something is wrong with the DB'})
+                return json.dumps(cursor.fetchone())
+    except Exception as e:    
+        return json.dumps({'error':'something is wrong with the DB'+repr(e)})
 
-
-
-@put('/update/<name>')
-def update(name):
+@put('/update/')
+def update():
+        myIdid = request.json.get("id")
+        fnewname = request.json.get("newName")
         try:
          with connection.cursor() as cursor:
-                # fname = request.json.get("firstName")
-                query = f'update  student set firstName  ="test" where firstname ="{name}"'
+                
+                query = f"update student set firstName = '{fnewname}' where id = {myIdid}"
+                print(query)
                 cursor.execute(query)
                 connection.commit()
-        except:    
-         return json.dumps({'error':'something is wrong with the DB'})       
+                students_query = f"SELECT * FROM student where id = {myIdid}"
+                cursor.execute(students_query)
+                return json.dumps(cursor.fetchone())
+        except Exception as e:    
+         return json.dumps({'error':'something is wrong with the DB'+repr(e)})       
+               
+                
+@delete('/delete_student')
+def dell_student():
+    myIdid = request.json.get("id")
+    try:
+        with connection.cursor() as cursor:
+            query=f'DELETE FROM student WHERE id="{myIdid}"'
+            cursor.execute(query)
+            connection.commit()
+            answer="item with id:" + myIdid +" removed from DB"
+            return answer
+    except Exception as e:    
+         return json.dumps({'error':'something is wrong with the DB'+repr(e)})    
+ 
+@route('/cohort/<year>')
+def get_cohort(year):
+        print(year)
+        print(type(year))
+        try:
+                with connection.cursor() as cursor:
+                        query = f'SELECT * FROM cohort WHERE cohort_year ="{year}"'
+                        cursor.execute(query)
+                return json.dumps(cursor.fetchall())
+        except  Exception as e:
+                return json.dumps({'error':'something is wrong with the DB'+repr(e)})
 
 run(host='localhost', port=7000, debug=True,reloader=True)
